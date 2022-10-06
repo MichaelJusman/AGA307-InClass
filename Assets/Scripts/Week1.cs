ae1f9fa19a14f61862ebe732e8be5f2a0c065697
MichaelJusman <107088338+MichaelJusman@users.noreply.github.com>
2022-10-06 13:09:09 +1100

    Initial commit

---
+++ Assets/Scripts/Week1.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Week1 : MonoBehaviour
+{
+    public int numberOne;
+    public int numberTwo;
+    public int numberThree;
+
+    public int health = 100;
+    public int bonus = 50;
+
+    public GameObject go;
+    public Camera cam;
+    Light ourLight;
+    
+    void Start()
+    {
+        ourLight = go.GetComponent<Light>();
+        ourLight.color = Color.green;
+        
+        
+        AddNumbers(numberOne, numberTwo);
+        AddNumbers(numberOne, numberThree);
+        AddNumbers(numberTwo, numberThree);
+        health = AddValues(health, bonus);
+        Debug.Log(health);
+    }
+
+    int AddValues(int _one, int _two)
+    {
+        return _one + _two;
+    }
+
+    /// <summary>
+    /// Use this to add two numbers together
+    /// </summary>
+    /// <param name="_one">The first number</param>
+    /// <param name="_two">The second number</param>
+    void AddNumbers(int _one, int _two)
+    {
+        int answer = _one + _two;
+        Debug.Log(answer);
+    }
+}
